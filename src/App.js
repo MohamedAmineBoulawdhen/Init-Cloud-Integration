53069245ce6c43681a3f74cca853b00e243e2c8d
Mohamed Amine Boulawdhen <medamineboulawdhen@gmail.com>
2023-03-22 21:51:48 +0100

    Integrate google maps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Map from "./components/map/Map";
+
 
 function App() {
+  const location = {
+    address: 'Tunis, Rawad',
+    lat: 36.910993,
+    lng: 10.187284,
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map location={location} zoomLevel={17} />
     </div>
   );
 }
